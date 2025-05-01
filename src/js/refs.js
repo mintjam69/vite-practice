9f2f18d88af09df5e07d6cd9793a8074d00ac64b
Taras Pererva <taraspererva@gmail.com>
2025-05-01 15:06:18 +0300

    do something

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+    form: document.querySelector(".login-form"),
+    inputEmail: document.querySelector("[name='email']"),
+    inputPassword: document.querySelector("[name='password']"),
+    buttonFirst: document.querySelector(".login-form button"),
+}
+
