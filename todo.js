7cbc437d6ce0631472cdca5d44f8684e20cedcea
mintjam69 <taraspererva@gmail.com>
2025-05-02 12:39:03 +0000

    Deploying to gh-pages from @ mintjam69/vite-practice@9e271bc42ef7f059d86c37bdf0c60ceb46f17c00 🚀

--- todo.js
+++ todo.js
@@ -1,2 +1,6 @@
-import{r as e,s as r}from"./assets/storage-BsKeap_u.js";/* empty css                      */import{i}from"./assets/vendor-I1I71QQ2.js";const a=[],n="tasks";e.taskForm.addEventListener("submit",m);function m(t){t.preventDefault();const s=t.target.elements.taskName.value.trim();if(s===""){i.warning({message:"Enter something for add",position:"topRight"});return}const o={text:s,id:Date.now()};a.push(o),r(n,a),e.taskForm.reset()}
+import{g as k,r as a,s as o}from"./assets/storage-BsKeap_u.js";/* empty css                      */import{i as m}from"./assets/vendor-I1I71QQ2.js";const n="tasks";let t=k(n)||[];a.taskList.innerHTML=r(t);a.taskForm.addEventListener("submit",d);a.taskList.addEventListener("click",u);function d(e){e.preventDefault();const s=e.target.elements.taskName.value.trim();if(s===""){m.warning({message:"Enter something for add",position:"topRight"});return}const i={text:s,id:Date.now()};t.push(i),o(n,t),a.taskList.innerHTML=r(t),a.taskForm.reset()}function u(e){e.target.nodeName==="BUTTON"&&(t=t.filter(s=>s.id!==+e.target.id),a.taskList.innerHTML=r(t),o(n,t))}function r(e){return e.map(({text:s,id:i})=>`
+  <li>
+  ${s} <button id="${i}">Delete</button>
+  </li>
+  `).join("")}
 //# sourceMappingURL=todo.js.map
