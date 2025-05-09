030de655f32aad76bd6aef7340337b877a166f88
Will IM <vitalii.mieliet@gmail.com>
2025-05-09 14:43:14 +0300

    Do task cats&dogs

--- src/js/module-11/main.js
+++ src/js/module-11/main.js
@@ -4,54 +4,67 @@
  * 1) Створити сторінку яка при завантаженні робить запит на бекенд і оримує
  *  всі доступні breeds. https://api.thecatapi.com/v1/breeds
  * Розмітка картки має виглядати так https://prnt.sc/Kkts8iqQmT91 (для стилізації використовуй наявні стилі)
- * 
+ *
  * 2) Додай кнопки, що буде робити запити за рандомними картинками котиків та собачок.
-* 3) Після запиту під кнопками відображається список карток з зображеннями котиків чи собачок.
+ * 3) Після запиту під кнопками відображається список карток з зображеннями котиків чи собачок.
  * 4) Під час запиту під кнопками відображається loader
  * 5) При кліку на рандомного котика/собачки відображай модальне вікно з зображенням тваринки
  */
 
-import { fetchAllCats, fetchRandomCats, fetchRandomDogs } from "./cats-api";
-import { createMarkup, createRandomMarkup, showElement, hideElement } from "./render-functions";
+import { fetchAllCats, fetchRandomCats, fetchRandomDogs } from './cats-api';
+import {
+  createMarkup,
+  createRandomMarkup,
+  showElement,
+  hideElement,
+  simpleLightbox,
+} from './render-functions';
 
 const container = document.querySelector('.card-container');
 const getCatsBtn = document.querySelector('.get-cats');
 const getDogsBtn = document.querySelector('.get-dogs');
 const loader = document.querySelector('.loader');
 
-fetchAllCats().then((res) => {
-    console.log(res)
+fetchAllCats().then(res => {
+  console.log(res);
 
-    container.innerHTML = createMarkup(res);
+  container.innerHTML = createMarkup(res);
 });
-
 
 getCatsBtn.addEventListener('click', getRandomCats);
 getDogsBtn.addEventListener('click', getRandomDogs);
 
-function getRandomCats (){
- showElement(loader);
-    fetchRandomCats().then(res => {
-        console.log(res)
+function getRandomCats() {
+  showElement(loader);
+  fetchRandomCats()
+    .then(res => {
+      console.log(res);
 
-        container.innerHTML = createRandomMarkup(res);
-    }).catch(error => {
-        console.log(error);
-    }).finally(() => {
-        hideElement(loader);
+      container.innerHTML = createRandomMarkup(res);
+
+      simpleLightbox.refresh();
     })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => {
+      hideElement(loader);
+    });
 }
 
-function getRandomDogs (){
-    showElement(loader);
-    fetchRandomDogs().then(res => {
-        console.log(res)
+function getRandomDogs() {
+  showElement(loader);
+  fetchRandomDogs()
+    .then(res => {
+      console.log(res);
 
-        container.innerHTML = createRandomMarkup(res);
-    }).catch(error => {
-        console.log(error);
-    }).finally(() => {
-        hideElement(loader);
+      container.innerHTML = createRandomMarkup(res);
+      simpleLightbox.refresh();
     })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => {
+      hideElement(loader);
+    });
 }
-
