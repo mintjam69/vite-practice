7706c8887e953499758ae73b7521d9aa85e32cfd
Will IM <vitalii.mieliet@gmail.com>
2025-05-13 15:00:59 +0300

    Add agination to gallery

--- src/js/module-12/gallery.js
+++ src/js/module-12/gallery.js
@@ -18,6 +18,8 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import { UnsplashAPI } from './UnsplashAPI';
 import { createGalleryCard } from './createGalleryCard';
+import Pagination from 'tui-pagination'; /* ES6 */
+import 'tui-pagination/dist/tui-pagination.min.css';
 
 import { showElement, hideElement } from '../module-11/render-functions';
 
@@ -25,17 +27,30 @@ const loader = document.querySelector('.loader');
 
 const galleryElem = document.querySelector('.gallery');
 const formElem = document.querySelector('.js-search-form');
+const container = document.getElementById('tui-pagination-container');
+const pagination = new Pagination(container, {
+  totalItems: 0,
+  itemsPerPage: 12,
+  visiblePages: 5,
+  page: 1,
+});
+const page = pagination.getCurrentPage();
 
 formElem.addEventListener('submit', onFormSubmit);
 
 const api = new UnsplashAPI();
 
-api.fetchPopularPhotos(1).then(data => {
+pagination.on('afterMove', getPopularPhotos);
+
+api.fetchPopularPhotos(page).then(data => {
   galleryElem.innerHTML = createGalleryCard(data.results);
+
+  pagination.reset(data.total);
 });
 
 async function onFormSubmit(event) {
   event.preventDefault();
+
   const inputValue = event.target.elements.query.value.trim();
 
   if (!inputValue) {
@@ -46,11 +61,14 @@ async function onFormSubmit(event) {
     return;
   }
 
+  pagination.off('afterMove', getPopularPhotos);
+  pagination.off('afterMove', getPhotosByQuery);
+
   api.query = inputValue;
 
   showElement(loader);
   try {
-    const data = await api.fetchPhotosByQuery(1);
+    const data = await api.fetchPhotosByQuery(page);
     if (data.results.length === 0) {
       iziToast.warning({
         message: 'Images not found. Try another query',
@@ -63,7 +81,11 @@ async function onFormSubmit(event) {
       message: `There are ${data.total} images`,
     });
 
+    pagination.reset(data.total);
+
     galleryElem.innerHTML = createGalleryCard(data.results);
+
+    pagination.on('afterMove', getPhotosByQuery);
   } catch (error) {
     console.log(error);
 
@@ -73,5 +95,27 @@ async function onFormSubmit(event) {
   } finally {
     hideElement(loader);
     formElem.reset();
+  }
+}
+
+async function getPopularPhotos(event) {
+  const currentPage = event.page;
+  try {
+    const data = await api.fetchPopularPhotos(currentPage);
+
+    galleryElem.innerHTML = createGalleryCard(data.results);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getPhotosByQuery(event) {
+  const currentPage = event.page;
+  try {
+    const data = await api.fetchPhotosByQuery(currentPage);
+
+    galleryElem.innerHTML = createGalleryCard(data.results);
+  } catch (error) {
+    console.log(error);
   }
 }
